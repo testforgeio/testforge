37d222417bc7f6d18fd9358c4dcfa431821797e7
testforge <testforge.io@gmail.com>
2024-11-15 19:57:05 +0200

    feat: add shadch

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://testforge.blog",
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
