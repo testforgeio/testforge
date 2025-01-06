81cac5bb1a554fa6c448d44cabbae5bed86dc8e8
testforge <testforge.io@gmail.com>
2025-01-07 00:52:36 +0200

    tailwind

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,9 +3,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://testforge.blog',
-    integrations: [mdx(), sitemap(), partytown()],
+    integrations: [
+      mdx(), 
+      sitemap(), 
+      partytown(),
+      react(),
+      tailwind({
+        applyBaseStyles: false,
+      })
+    ],
 });
