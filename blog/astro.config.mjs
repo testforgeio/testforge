c6cc0d325ee89a3b17115b2faba9d6ff0ae44b26
testforge <testforge.io@gmail.com>
2025-01-17 23:32:23 +0200

    ga

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://testforge.blog',
-	integrations: [mdx(), sitemap()],
+    site: 'https://testforge.blog',
+    integrations: [mdx(), sitemap(), partytown()],
 });
