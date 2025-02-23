e35cdf18d1e4fd8440292e3e4625a99c144942a9
testforge <testforge.io@gmail.com>
2025-02-23 16:15:27 +0200

    tools

--- blog/tailwind.config.cjs
+++ blog/tailwind.config.cjs
@@ -12,70 +12,74 @@ module.exports = {
   darkMode: ["selector", "[data-theme='dark']"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    // Remove the following screen breakpoint or add other breakpoints
-    // if one breakpoint is not enough for you
-    screens: {
-      sm: "640px",
-    },
-
-    extend: {
-      textColor: {
-        skin: {
-          base: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-          inverted: withOpacity("--color-fill"),
-        },
-      },
-      backgroundColor: {
-        skin: {
-          fill: withOpacity("--color-fill"),
-          accent: withOpacity("--color-accent"),
-          inverted: withOpacity("--color-text-base"),
-          card: withOpacity("--color-card"),
-          "card-muted": withOpacity("--color-card-muted"),
-        },
-      },
-      outlineColor: {
-        skin: {
-          fill: withOpacity("--color-accent"),
-        },
-      },
-      borderColor: {
-        skin: {
-          line: withOpacity("--color-border"),
-          fill: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-        },
-      },
-      fill: {
-        skin: {
-          base: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-        },
-        transparent: "transparent",
-      },
-      stroke: {
-        skin: {
-          accent: withOpacity("--color-accent")
-        }
-      },
-      fontFamily: {
-        mono: ["IBM Plex Mono", "monospace"],
-      },
-
-      typography: {
-        DEFAULT: {
-          css: {
-            pre: {
-              color: false,
-            },
-            code: {
-              color: false,
-            },
-          },
-        },
-      },
-    },
+  	screens: {
+  		sm: '640px'
+  	},
+  	extend: {
+  		textColor: {
+  			skin: {
+  				base: 'withOpacity("--color-text-base")',
+  				accent: 'withOpacity("--color-accent")',
+  				inverted: 'withOpacity("--color-fill")'
+  			}
+  		},
+  		backgroundColor: {
+  			skin: {
+  				fill: 'withOpacity("--color-fill")',
+  				accent: 'withOpacity("--color-accent")',
+  				inverted: 'withOpacity("--color-text-base")',
+  				card: 'withOpacity("--color-card")',
+  				'card-muted': 'withOpacity("--color-card-muted")'
+  			}
+  		},
+  		outlineColor: {
+  			skin: {
+  				fill: 'withOpacity("--color-accent")'
+  			}
+  		},
+  		borderColor: {
+  			skin: {
+  				line: 'withOpacity("--color-border")',
+  				fill: 'withOpacity("--color-text-base")',
+  				accent: 'withOpacity("--color-accent")'
+  			}
+  		},
+  		fill: {
+  			skin: {
+  				base: 'withOpacity("--color-text-base")',
+  				accent: 'withOpacity("--color-accent")'
+  			},
+  			transparent: 'transparent'
+  		},
+  		stroke: {
+  			skin: {
+  				accent: 'withOpacity("--color-accent")'
+  			}
+  		},
+  		fontFamily: {
+  			mono: [
+  				'IBM Plex Mono',
+  				'monospace'
+  			]
+  		},
+  		typography: {
+  			DEFAULT: {
+  				css: {
+  					pre: {
+  						color: false
+  					},
+  					code: {
+  						color: false
+  					}
+  				}
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  	}
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 };
